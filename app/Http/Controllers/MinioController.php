df02774823b72a57301bfbbc197e0685289d9b76
Ahrorbek Davronov <axrorbektatu@gmail.com>
2023-11-16 13:26:58 +0500

    s3-minio

--- app/Http/Controllers/MinioController.php
+++ app/Http/Controllers/MinioController.php
@@ -8,12 +8,14 @@ use Illuminate\Support\Facades\Storage;
 class MinioController extends Controller
 {
     public function get(){
-        $data = Storage::disk('minio')->publicUrl('hello.json');
+        $data = Storage::disk('minio')->publicUrl('17625506_d5db_qz19_201126-ai (14).png');
+        // $data = Storage::disk('minio')->temporaryUrl('17625506_d5db_qz19_201126-ai (14).png', \Carbon\Carbon::now()->addMinutes(1));
         return response()->json($data);
     }
 
-    public function set(){
-        $data = Storage::disk('minio')->write('hello.json', '{"hello": "world"}');
+    public function set(Request $request){
+        $data = Storage::disk('minio')->write($request->file('file')->getClientOriginalName(), $request->file('file'));
+        // $data = Storage::disk('minio')->writeStream($request->file('file')->getClientOriginalName(), $request->file('file'));
         return response()->json($data);
     }
 }
